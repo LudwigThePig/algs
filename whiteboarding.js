c3e53f284f971d3d6e1c6d2a607ccfa1f4609040
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-12 09:13:15 -0600

    Add to LL

--- whiteboarding.js
+++ whiteboarding.js
@@ -180,7 +180,31 @@ const srujsSpaceDestroyer = (str) => {
   return result;
 }
 
-print('Mr%20John%20Smith', srujsSpaceDestroyer('Mr John Smith'))
-print('Mr%20John%20Smith', srujsSpaceDestroyer('Mr John Smith '))
-print('', srujsSpaceDestroyer(''))
-print('     ', srujsSpaceDestroyer('           '))
+// print('Mr%20John%20Smith', srujsSpaceDestroyer('Mr John Smith'));
+// print('Mr%20John%20Smith', srujsSpaceDestroyer('Mr John Smith '));
+// print('', srujsSpaceDestroyer(''));
+// print('     ', srujsSpaceDestroyer('           '));
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+var addTwoNumbers = function(l1, l2) {
+  const before = new ListNode();
+  let tail = before;
+  let c = 0;
+
+  while (l1 || l2 || c) {
+    const v1 = l1 ? l1.val : 0;
+    const v2 = l2 ? l2.val : 0;
+    const v = v1+v2+c;
+
+    tail.next = new ListNode(v%10);
+    tail = tail.next;
+    c = v >= 10 ? 1 : 0;
+    l1 = l1&&l1.next;
+    l2 = l2&&l2.next;
+  }
+
+  return before.next;
+};
