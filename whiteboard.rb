1869ffe72e55d37bfe46f4a5594a19cd3791517c
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-17 11:55:26 -0600

    remove_duplicates in ruby

--- whiteboard.rb
+++ whiteboard.rb
@@ -5,3 +5,26 @@ def reverse(x)
   (res < -2147483648 || res > 2147483647) ? 0 : res
 end
 
+
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+# Cheeky answer
+def remove_duplicates(nums)
+  nums.uniq!
+  nums.length
+end
+
+def remove_duplicates2(nums)
+  if nums.empty? 
+      return 0 
+  end
+  uniq = 1
+  i = 0
+  while uniq < nums.length
+      if nums[i] != nums[uniq]
+          i += 1
+          nums[i] = nums[uniq]
+      end        
+      uniq += 1
+  end
+  i + 1
+end
