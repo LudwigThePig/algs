5b7f6bb929b49f8df2f24c8e7854cc518a852261
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-28 09:27:51 -0600

    regex matching func

--- whiteboarding.js
+++ whiteboarding.js
@@ -457,3 +457,27 @@ var longestPalindrome = function(s) {
   }
   return longest;
 };
+
+
+
+var isMatch = function(s, p) {
+  if (s.length === 0) {
+
+    if (p.length === 0) return true;
+    if (p[1] === '*') return isMatch(s, p.slice(2));
+    return false;
+
+  } else {
+    
+    if (p[1] === '*') {
+      if (p[0] === s[0] || p[0] === '.') {
+        return isMatch(s.slice(1), p) || isMatch(s, p.slice(2));
+      }
+      return isMatch(s, p.slice(2));
+    }
+    
+    if (s[0] === p[0] || p[0] === '.') return isMatch(s.slice(1), p.slice(1));
+    return false;
+
+  }
+};
