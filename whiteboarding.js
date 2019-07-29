2ff95c9e1185a6e958d30cd5797d8e4b34630a3e
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-28 19:10:28 -0600

    math.pow in javascript

--- whiteboarding.js
+++ whiteboarding.js
@@ -481,3 +481,22 @@ var isMatch = function(s, p) {
 
   }
 };
+
+
+// https://leetcode.com/problems/powx-n/
+var myPow = function(x, n) {
+  if (n === 0) return 1;
+  if (n === 1) return x;
+  if (x === 0) return 0;
+  
+  if (n > 0) {
+     if (n % 2 === 0) {
+        const half = myPow(x, n/2);
+        return half * half;
+     } else {
+       return x * myPow(x, n-1);
+     }
+  } else {
+    return myPow(1/x, -n);
+  }
+};
