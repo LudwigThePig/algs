85d25a3d95f102d256d4709d0eb2c54bb87ad829
Morgan Galvin <morgan@ludwigthepig.com>
2019-11-10 11:34:18 -0700

    solution: largest rect area, brute force

--- whiteboarding.js
+++ whiteboarding.js
@@ -1003,3 +1003,31 @@ var countBits = function(num) {
   }
   return res;
 };
+
+
+
+// https://leetcode.com/problems/largest-rectangle-in-histogram/submissions/
+var largestRectangleArea = function(heights) {
+  let max = 0;
+  for (let i = 0; i< heights.length; i++) {
+      const base = heights[i];
+      const arr = [heights[i]];
+      
+      // go right
+      for (let j = i + 1; j < heights.length; j++) {
+          if (heights[j] < base) break;
+          arr.push(heights[j])
+      }
+      // go left
+      for (let j = i - 1; j >= 0; j--) {
+          if (heights[j] < base) break;
+          arr.push(heights[j])
+      }
+      
+      // calc and compare
+      const area = arr.length * base
+      if (area > max) max = area;
+  }
+  
+  return max;
+};
