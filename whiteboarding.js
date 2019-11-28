fa88861cb40a569bfbf6cc4184f6507e59f3f905
Morgan Galvin <morgan@ludwigthepig.com>
2019-11-27 17:00:17 -0700

    solution: minPathSum

--- whiteboarding.js
+++ whiteboarding.js
@@ -1078,3 +1078,36 @@ var largestRectangleArea = function(height) {
   }
   return maxArea;
 };
+
+
+
+
+
+// https://leetcode.com/problems/minimum-path-sum
+var minPathSum = function(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  
+  
+  for(let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+          // at start, do nothing
+          if (row === 0 && col === 0) continue;
+
+          if (row !== 0 && col !== 0) {
+              // in the middle
+              grid[row][col] += Math.min(grid[row - 1][col], grid[row][col - 1]);
+
+          } else if (row === 0) { 
+              // up agains the ceiling
+              grid[row][col] += grid[row][col - 1];
+
+          } else if (col === 0) { 
+              // up agains the left wall
+              grid[row][col] += grid[row - 1][col];
+          }
+      }    
+  }
+
+  return grid[rows - 1][cols - 1]
+};
