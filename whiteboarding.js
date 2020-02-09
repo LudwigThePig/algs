2186b826fe6897a65fe5219c1b5d25572770aef9
Morgan Galvin <morgan@ludwigthepig.com>
2020-02-09 13:52:41 -0700

    solution: nth Fib (dp)

--- whiteboarding.js
+++ whiteboarding.js
@@ -1800,3 +1800,19 @@ var climbStairs = function(n) {
 
   return dp[n - 1];
 };
+
+
+
+// https://leetcode.com/problems/fibonacci-number/submissions/
+var fib = function(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  const dp = [0, 1];
+  
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  
+  return dp[n];
+};
