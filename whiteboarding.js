dbb00f16844d6719c364f0fe6e5366d10ed1bf74
Morgan Galvin <morgan@ludwigthepig.com>
2020-04-18 13:50:31 -0600

    solution: same tree

--- whiteboarding.js
+++ whiteboarding.js
@@ -2211,3 +2211,18 @@ var lastStoneWeight = function(stones) {
   }
   return stones[0]
 };
+
+
+
+
+
+
+// https://leetcode.com/problems/same-tree/submissions/
+var isSameTree = function(p, q) {
+  if (!p && !q) return true;
+  if ((!p && q) 
+      || (p && !q) 
+      || (p.val !== q.val)) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
