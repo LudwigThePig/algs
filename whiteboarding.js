0a677f8579b7ae70561526cb6efe4c8ec389d35c
Morgan Galvin <morgan@ludwigthepig.com>
2020-04-26 12:01:33 -0600

    solution: longestCommonSubsequence

--- whiteboarding.js
+++ whiteboarding.js
@@ -2604,3 +2604,27 @@ var canJump = function(nums) {
 
   return target === 0;
 };
+
+
+
+// https://leetcode.com/problems/longest-common-subsequence/
+var longestCommonSubsequence = function(text1, text2) {
+  
+  const dp = new Array(text1.length + 1) // Matrix, rows are t2, cols t1
+    .fill(0)
+    .map(() => new Array(text2.length + 1).fill(0));
+
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      const up = dp[i-1][j];
+      const left = dp[i][j-1];
+      const diag = dp[i-1][j-1];
+      
+      dp[i][j] = (text1[i-1] === text2[j-1]) 
+          ? diag + 1
+          : Math.max(up, left, diag);
+    }
+  }
+  
+  return dp[text1.length][text2.length]
+};
